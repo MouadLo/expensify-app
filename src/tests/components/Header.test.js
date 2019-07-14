a058bf1efbfc9987ceac3b1842ffbabd470c618d
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2019-07-13 21:27:02 -0400

    setup format for amount & createdAt

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,11 @@
-
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 import  Header  from '../../components/Header';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(toJSON(wrapper)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
+
+
 
